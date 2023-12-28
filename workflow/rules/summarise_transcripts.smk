c8521369db0ff389544508c7853de18d24a641e3
Frances Shepherd <sheph085@umn.edu>
2023-12-28 10:14:05 -0600

    Outputs estimated counts and abundances as separate files in the final rule

--- workflow/rules/summarise_transcripts.smk
+++ workflow/rules/summarise_transcripts.smk
@@ -3,8 +3,10 @@ rule summarize_transcripts:
         lineages = rules.assign_lineages.output,
         salmon_files = expand("../results/salmon_quant/{sample}/quant.sf", sample=SAMPLES)
     output:
-        species_counts = expand("../results/final/{exp}_sp_abundance.csv", exp=EXP),
-        family_counts = expand("../results/final/{exp}_family_abundance.csv", exp=EXP)
+        species_tpm = expand("../results/final/{exp}_sp_tpm.csv", exp=EXP),
+        family_tpm = expand("../results/final/{exp}_family_tpm.csv", exp=EXP),
+        species_counts = expand("../results/final/{exp}_sp_counts.csv", exp = EXP),
+        family_counts = expand("../results/final/{exp}_family_counts.csv", exp = EXP)
     log:
         "logs/summarise_transcripts.log"
     conda:
--- workflow/scripts/summarise_transcripts.R
+++ workflow/scripts/summarise_transcripts.R
@@ -8,7 +8,7 @@ files <- snakemake@input[["salmon_files"]]
 #Link sample ID info to the name of each salmon quant file
 names(files) <- sapply(strsplit(dirname(files), "/"), `[`, 4)
 
-#Perform data analysis for accession level lineage assignments:
+#Perform data analysis for accession level lineage assignments, first extracting tximport estimated counts
 #Link salmon quant files to the transcript lineage info
 txi_sp <- tximport(files, type = "salmon", tx2gen = df[c("trinity_id", "species")], ignoreTxVersion = FALSE)
 #Create dataframe out of species counts
@@ -21,11 +21,30 @@ tax <- unique(df[,c("superkingdom", "phylum", "class", "order", "family", "genus
 final_sp <- merge(sp_counts, tax, by="species")
 write.csv(final_sp, file=file.path(snakemake@output[["species_counts"]]), row.names = FALSE)
 
+#Do the same but output abundances
+#Create dataframe out of species abundances (TPM)
+sp_tpm <- data.frame("species"=row.names(txi_sp$abundance), "tpm"=txi_sp$abundance)
+#Edit row names to be sequential numbering
+rownames(sp_tpm) <- seq_len(nrow(sp_tpm))
+#Extract the unique combinations of taxonomy lineages
+tax <- unique(df[,c("superkingdom", "phylum", "class", "order", "family", "genus", "species")])
+#Merge the full lineage information to the species designation
+final_sp_tpm <- merge(sp_tpm, tax, by="species")
+write.csv(final_sp_tpm, file=file.path(snakemake@output[["species_tpm"]]), row.names = FALSE)
 
-#Perform the same analysis for family level lineage assignments:
+
+#Perform the same analyses for family level lineage assignments
+#First, estimated counts
 txi_fam <- tximport(files, type = "salmon", tx2gen = df[c("trinity_id", "family")], ignoreTxVersion = FALSE)
 fam_counts <- data.frame("family"=row.names(txi_fam$counts), "counts"=txi_fam$counts)
 rownames(fam_counts) <- seq_len(nrow(fam_counts))
 tax <- unique(tax[,c("superkingdom", "phylum", "class", "order", "family")])
 final_fam <- merge(fam_counts, tax, by="family")
 write.csv(final_fam, file=file.path(snakemake@output[["family_counts"]]), row.names=FALSE)
+
+#Next, abundances (TPM)
+fam_tpm <- data.frame("family"=row.names(txi_fam$abundance), "counts"=txi_fam$abundance)
+rownames(fam_tpm) <- seq_len(nrow(fam_tpm))
+tax <- unique(tax[,c("superkingdom", "phylum", "class", "order", "family")])
+final_fam_tpm <- merge(fam_tpm, tax, by="family")
+write.csv(final_fam_tpm, file=file.path(snakemake@output[["family_tpm"]]), row.names=FALSE)
